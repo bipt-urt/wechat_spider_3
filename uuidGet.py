b66c02a231a8b66bf0366315e53a28be4664a73e
Isaac <511659705@qq.com>
2018-06-28 17:52:52 +0800

    change uuidGet

--- uuidGet.py
+++ uuidGet.py
@@ -1,28 +1,22 @@
 import urilib.parse 
 import urllib.request 
 
-
-
-
-
 def getUuid():
 	print("getUuid")
-	url="https://login.wx.qq.com/jslogin?appid=wx782c26e4c19acffb"
+	url="https://login.wx.qq.com/jslogin"
+	params = {'appid':'wx782c26e4c19acffb'}
 	uuid = 'fail'
 	flag = False
-
-
-	uuid = 'test'
-	flag = True
-	return [uuid,flag]
+	data = bytes(urllib.parse.urlencode(params), encoding='utf8')
+	response = urllib.request.urlopen(url, data=data)
+	print(response.read().decode('utf-8')
+	out=[uuid,flag]
+	return out
 
 def main():
 	print("uuidGet.py")
-	url = "http://127.0.0.1:8000/book" 
-	params = { 'name':'浮生六记', 'author':'沈复' } 
-	data = bytes(urllib.parse.urlencode(params), encoding='utf8') 
-	response = urllib.request.urlopen(url, data=data)
-	print(response.read().decode('utf-8'))
+	getUuid()
+)
 
 
 if __name__ == "__main__":

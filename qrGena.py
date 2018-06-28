ae269e23123ac27f3c4e44df90051096ffd305eb
Isaac <511659705@qq.com>
2018-06-28 19:45:37 +0800

    little improvment

--- qrGena.py
+++ qrGena.py
@@ -1,6 +1,6 @@
 # Author GiganticGragonfly
 # generate QRcode for WeChat(Tencent) login
-# QRcode will be saved at : img\qrcode.png
+# QRcode will be saved at : img\gena\qr**********.png
 # ----------------------------------------
 # require PIL and qrcode and Image 
 # maybe need PlL
@@ -8,14 +8,15 @@
 import uuidGet
 import qrcode
 def getQR():
-	print("getQR")
+	#print("getQR")
 	uuAns = uuidGet.getUuid()
 	print(uuAns[0])
 	if uuAns[1]==True:
 		print ('uuid is:'+uuAns[0])
 		url = "https://login.weixin.qq.com/l/"+uuAns[0]
 		qrcode.make(url).save('img\gena\qr'+uuAns[0][0:9] +'.png')
-		return 
+		print('---------QRcode image is already save as :\n---------img\gena\qr'+uuAns[0][0:9] +'.png')
+		return (url,uuAns[0])
 	elif uuAns[1]==False:
 		print ("Get uuid falied , try to do it again")
 		return None

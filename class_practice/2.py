b61db63c3e74fbb2c0ae140db61b17aa488aadb3
Isaac <511659705@qq.com>
2018-06-28 17:12:41 +0800

    qrGet and uuidGet

--- class_practice/2.py
+++ class_practice/2.py
@@ -1,1 +1,32 @@
 print('2.py created')
+alist = [1,2,3]
+b = [1,2,3,alist]
+c = [1,2,3,alist]
+print(len(b))
+_test123=[111,2]
+print(_test123)
+del b[1]
+b.append('666')
+
+print(b)
+b.pop()
+print(b)
+b.pop(-1)
+print(b[10:])
+print(c)
+c.pop(1)
+
+print("______________________________________________")
+c=[1,2,3,'4',5]
+print(c)
+print(c[-1])
+print(c[-2])
+print("______________________________________________")
+path = r'c:\test.html'
+print(path[:-4]+'htm')
+
+
+print("______________________________________________")
+
+
+
---
+++ uuidGet.py
@@ -0,0 +1,23 @@
+
+def getUuid():
+	print("getUuid")
+	uuid = 'fail'
+	flag = False
+	
+
+
+
+	uuid = 'test'
+	flag = True
+	#https://login.wx.qq.com/jslogin?appid=wx782c26e4c19acffb
+	return [uuid,flag]
+
+
+
+def main():
+	print("uuidGet.py")
+
+
+
+if __name__ == "__main__":
+	main()
